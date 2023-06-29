6d79f8a68d68f01d7160dd534ca04f2357b3a302
Phuc Ho <hotrongphuc643@gmail.com>
2023-06-30 00:50:31 +0700

    update

--- src/bouncing-ball/GameManager.ts
+++ src/bouncing-ball/GameManager.ts
@@ -1,7 +1,15 @@
 import { config } from './config'
 
 export class GameManager {
+    public static instance: GameManager | null = null
     constructor() {
         new Phaser.Game(config)
+    }
+
+    public static getInstance() {
+        if (!GameManager.instance) {
+            GameManager.instance = new GameManager()
+        }
+        return GameManager.instance
     }
 }
--- src/game.ts
+++ src/game.ts
@@ -3,7 +3,7 @@ import { GameManager } from './bouncing-ball/GameManager'
 class Game {
     constructor() {
         console.log('Game created')
-        new GameManager()
+        GameManager.getInstance()
     }
 }
 
