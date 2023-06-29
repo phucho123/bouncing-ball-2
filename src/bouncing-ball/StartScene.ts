8c044fe56694d922a073c5e139fcc38b743b3f6e
Phuc Ho <hotrongphuc643@gmail.com>
2023-06-30 00:46:12 +0700

    update

--- src/bouncing-ball/StartScene.ts
+++ src/bouncing-ball/StartScene.ts
@@ -9,12 +9,50 @@ export class StartScene extends Phaser.Scene {
         super({ key: 'Start Scene' })
     }
 
-    preload() {
+    public preload() {
         this.load.image('start-button', 'assets/images/start-button.png')
         this.load.image('shop-button', 'assets/images/shop.png')
     }
 
-    create() {
+    public create() {
+        this.createButton()
+        this.text = this.add
+            .text(60, 100, 'Bouncing\n   Ball 2', {
+                fontSize: `${(64 * CANVAS_WIDTH) / 400}px`,
+                fontFamily: 'Arial',
+                color: '#000000',
+                testString: '1234y',
+            })
+            .setStroke('black', 1)
+            .setOrigin(0.5)
+            .setPosition(CANVAS_WIDTH / 2, (CANVAS_HEIGHT / 2) * 0.5)
+        this.getDataFromStorage()
+    }
+
+    public getDataFromStorage() {
+        const chosenBall = localStorage.getItem('chosenBall')
+        if (chosenBall) {
+            ShopScene.chosenBall = parseInt(chosenBall)
+        } else {
+            ShopScene.chosenBall = 0
+        }
+        const playerGem = localStorage.getItem('totalGem')
+        if (playerGem) {
+            ShopScene.playerGem = parseInt(playerGem)
+        } else {
+            ShopScene.playerGem = 0
+        }
+        const boughtBall = localStorage.getItem('boughtBall')
+        if (!boughtBall) {
+            localStorage.setItem('boughtBall', '0')
+        }
+        const tmp = localStorage.getItem('highScore')
+        if (tmp != null) {
+            PlayScene.highScore = parseInt(tmp)
+        } else PlayScene.highScore = 0
+    }
+
+    public createButton() {
         const startButton = this.add
             .image(CANVAS_WIDTH / 2, CANVAS_HEIGHT / 2, 'start-button')
             .setScale(0.2)
@@ -68,37 +106,5 @@ export class StartScene extends Phaser.Scene {
         shopButton.on('pointerdown', () => {
             this.scene.switch('Shop Scene')
         })
-
-        this.text = this.add
-            .text(60, 100, 'Bouncing\n   Ball 2', {
-                fontSize: `${(64 * CANVAS_WIDTH) / 400}px`,
-                fontFamily: 'Arial',
-                color: '#000000',
-                testString: '1234y',
-            })
-            .setStroke('black', 1)
-            .setOrigin(0.5)
-            .setPosition(CANVAS_WIDTH / 2, (CANVAS_HEIGHT / 2) * 0.5)
-
-        const chosenBall = localStorage.getItem('chosenBall')
-        if (chosenBall) {
-            ShopScene.chosenBall = parseInt(chosenBall)
-        } else {
-            ShopScene.chosenBall = 0
-        }
-        const playerGem = localStorage.getItem('totalGem')
-        if (playerGem) {
-            ShopScene.playerGem = parseInt(playerGem)
-        } else {
-            ShopScene.playerGem = 0
-        }
-        const boughtBall = localStorage.getItem('boughtBall')
-        if (!boughtBall) {
-            localStorage.setItem('boughtBall', '0')
-        }
-        const tmp = localStorage.getItem('highScore')
-        if (tmp != null) {
-            PlayScene.highScore = parseInt(tmp)
-        } else PlayScene.highScore = 0
     }
 }
